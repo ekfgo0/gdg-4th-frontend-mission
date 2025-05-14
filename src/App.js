438f84ce78155cce23d944dde5a45c8f08926614
ekfgo0 <anstjsdud0306@gmail.com>
2025-05-14 17:27:33 +0900

    React 자기소개 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    console.log("안녕하세요! 저는 문선영입니다.");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>안녕하세요! 저는 문선영입니다.</h1>
+      <img
+        src="/my_photo.jpeg"
+        alt="내 사진"
+        style={{ width: '150px', borderRadius: '50%' }}
+      />
+      <p>홍익대학교 컴퓨터공학과 재학중</p>
+      <p>25세</p>
+      <ul>
+        <li>좋아하는 동물: 강아지</li>
+        <li>좋아하는 영화: 이터널 션샤인</li>
+        <li>좋아하는 음식: 부대찌개</li>
+      </ul>
+      <button onClick={handleClick}>인사하기</button>
     </div>
   );
 }
